5979dd266f2b29454c6407c90d4635e9adf2d295
Prasad Hadbe <Prasadahadbe313@gmail.com>
2023-06-09 12:16:32 +0530

    .

--- src/components/about/Info.js
+++ src/components/about/Info.js
@@ -9,7 +9,7 @@ const Info = () => {
         <span className="about__subtitle">1 Years+</span>
       </div>
       <div className="about__box">
-        <i class="bx bx-award  about__icon"></i>
+        <i class="bx bx-award about__icon"></i>
         <h3 className="about__title">Experties</h3>
         <span className="about__subtitle">Mobile & Web</span>
       </div>
